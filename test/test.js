e88726bbc99cba7d1188c67a379ca616c7d8c359
gobwas <gobwas@gmail.com>
2015-06-11 21:34:36 +0300

    test

--- test/test.js
+++ test/test.js
@@ -9,4 +9,13 @@ describe('lodashsh node module', function () {
 
 		assert(usage[0] == "forEach", 'It not works =(');
 	});
+
+	it('must have at least two test', function () {
+		var usage;
+
+		usage = lodashsh("_ = require('lodash'); chain = _.chain(); chain.forEach()");
+
+		assert(usage[0] == "chain", 'It not works =(');
+		assert(usage[1] == "forEach", 'It not works =(');
+	});
 });
